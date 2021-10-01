5530ef263ba82a7529158f11d8edd8044e61546e
Trevor Steil <steil1@llnl.gov>
2021-10-01 14:56:28 -0700

    Changes performance/disjoint_set_union_chain to use unions that are generated in a random order

--- performance/disjoint_set_union_chain.cpp
+++ performance/disjoint_set_union_chain.cpp
@@ -3,6 +3,7 @@
 //
 // SPDX-License-Identifier: MIT
 
+#include <random>
 #include <ygm/comm.hpp>
 #include <ygm/container/disjoint_set.hpp>
 #include <ygm/utility.hpp>
@@ -16,8 +17,14 @@ int main(int argc, char **argv) {
   }
 
   size_t num_unions = atoll(argv[1]);
+  int    num_trials = 5;
+
+  if (argc > 2) {
+    num_trials = atoi(argv[2]);
+  }
 
   world.cout0("Global unions: ", num_unions);
+  world.cout0("Performing unions in random order");
 
   size_t num_local_unions =
       num_unions / world.size() + (world.rank() < num_unions % world.size());
@@ -25,33 +32,38 @@ int main(int argc, char **argv) {
       (num_unions / world.size()) * world.rank() +
       std::min<size_t>(world.rank(), num_unions % world.size());
 
-  /*****************************
-   * Experiment with unions incrementing
-   ****************************/
-  {
-    world.cout0("**** Forward unions ****");
+  std::vector<size_t> my_unions;
+
+  for (size_t i = local_offset; i < local_offset + num_local_unions; ++i) {
+    my_unions.push_back(i);
+  }
+
+  std::random_device rd;
+  std::mt19937       g(rd());
+
+  double cumulative_union_time{0.0};
+  double cumulative_compress_time{0.0};
+
+  for (int trial = 0; trial < num_trials; ++trial) {
+    world.cout0("\n********** Trial ", trial + 1, " **********");
     ygm::container::disjoint_set<size_t> dset(world);
 
-    /*****************************
-     * Time unions
-     ****************************/
+    std::shuffle(my_unions.begin(), my_unions.end(), g);
+
     world.barrier();
 
     ygm::timer union_timer{};
 
-    // Perform unions
-    for (size_t i = local_offset; i < local_offset + num_local_unions; ++i) {
-      dset.async_union(i, i + 1);
+    for (const auto &low_value : my_unions) {
+      dset.async_union(low_value, low_value + 1);
     }
 
     world.barrier();
 
-    world.cout0("Performed ", num_unions, " unions in ", union_timer.elapsed(),
-                " seconds");
+    double union_time = union_timer.elapsed();
+    world.cout0("Union time: ", union_time);
+    cumulative_union_time += union_time;
 
-    /*****************************
-     * Time all_compress
-     ****************************/
     world.barrier();
 
     ygm::timer compress_timer{};
@@ -60,48 +72,14 @@ int main(int argc, char **argv) {
 
     world.barrier();
 
-    world.cout0("Performed all_compress on ", num_unions, " items in ",
-                compress_timer.elapsed(), " seconds");
+    double compress_time = compress_timer.elapsed();
+    world.cout0("Compress time: ", compress_time);
+    cumulative_compress_time += compress_time;
   }
 
-  /*****************************
-   * Experiment with unions decrementing
-   ****************************/
-  {
-    world.cout0("\n**** Backward unions ****");
-    ygm::container::disjoint_set<size_t> dset(world);
-
-    /*****************************
-     * Time unions
-     ****************************/
-    world.barrier();
-
-    ygm::timer union_timer{};
-
-    // Perform unions
-    for (size_t i = local_offset + num_local_unions; i > local_offset; --i) {
-      dset.async_union(i - 1, i);
-    }
-
-    world.barrier();
-
-    world.cout0("Performed ", num_unions, " unions in ", union_timer.elapsed(),
-                " seconds");
-
-    /*****************************
-     * Time all_compress
-     ****************************/
-    world.barrier();
-
-    ygm::timer compress_timer{};
-
-    dset.all_compress();
-
-    world.barrier();
-
-    world.cout0("Performed all_compress on ", num_unions, " items in ",
-                compress_timer.elapsed(), " seconds");
-  }
+  world.cout0("\n********** Summary **********");
+  world.cout0("Average union time: ", cumulative_union_time / num_trials);
+  world.cout0("Average compress time: ", cumulative_compress_time / num_trials);
 
   return 0;
 }
