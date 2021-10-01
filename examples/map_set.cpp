03baa0692aef83296740ea165b5e83b08bf3ae48
Trevor Steil <steil1@llnl.gov>
2021-10-01 12:28:00 -0700

    Fixes example/map_set.cpp to compile and adds to CMake

--- examples/map_set.cpp
+++ examples/map_set.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char** argv) {
   ygm::comm world(&argc, &argv);
 
-  ygm::container::set<std::string> str_set(world);
+  ygm::container::set<std::string>              str_set(world);
   ygm::container::map<std::string, std::string> str_map(world);
 
   if (world.rank() == 0) {
@@ -25,8 +25,7 @@ int main(int argc, char** argv) {
     str_map.async_set("strawberry", "raspberry");
   }
 
-  str_set.all_for_all(
-      [](auto k) { std::cout << "str_set: " << k << std::endl; });
+  str_set.for_all([](auto k) { std::cout << "str_set: " << k << std::endl; });
 
   str_map.for_all([](auto kv) {
     std::cout << "str_map: " << kv.first << " -> " << kv.second << std::endl;
