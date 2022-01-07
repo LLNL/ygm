ac326321815fac83961009424116ab1900a82b7d
Trevor Steil <trevor.steil1@gmail.com>
2022-01-07 15:42:17 -0800

    Switching to webgraph_spmv.cpp as new alg_spmv.cpp

--- examples/alg_spmv.cpp
+++ examples/alg_spmv.cpp
@@ -3,136 +3,80 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include <fstream>
 #include <iostream>
-#include <iomanip>
 
-#include <math.h>
+#include <string>
+#include <vector>
 #include <ygm/comm.hpp>
-
-#include <ygm/container/map.hpp>
 #include <ygm/container/experimental/maptrix.hpp>
+#include <ygm/container/map.hpp>
+#include <ygm/io/line_parser.hpp>
+#include <ygm/utility.hpp>
 
 int main(int argc, char **argv) {
-
   ygm::comm world(&argc, &argv);
 
-  using map_type      = ygm::container::map<std::string, double>;
-  using maptrix_type  = ygm::container::experimental::maptrix<std::string, double>;
-  namespace ns_spmv   = ygm::container::experimental::detail::algorithms;
+  using map_type     = ygm::container::map<size_t, int>;
+  using maptrix_type = ygm::container::experimental::maptrix<size_t, int>;
 
-  map_type my_map(world);
-  maptrix_type my_maptrix(world);
-  
-  auto my_map_ptr     = my_map.get_ygm_ptr();
-  auto my_maptrix_ptr = my_maptrix.get_ygm_ptr(); 
+  namespace ns_spmv = ygm::container::experimental::detail::algorithms;
 
-  if(argc == 1) {
+  if (argc == 1) {
     std::cout << "Expected parameter arguments, exiting.." << std::endl;
     exit(0);
   }
 
-  std::string m_name = argv[1];
-  std::string v_name = argv[2];
-
-  std::ifstream matfile(m_name);
-  std::ifstream vecfile(v_name);
-  double value;
-  std::string key1, key2;
-  if (world.rank0()) {
-
-    while (matfile >> key1 >> key2 >> value) {
-      my_maptrix.async_insert(key1, key2, value);
-    }
-
-    while (vecfile >> key1 >> value) {
-      my_map.async_insert(key1, value);
-    }
+  std::vector<std::string> mat_files({argv[1]});
+  std::vector<std::string> vec_files;
+  bool                     read_vec{false};
+  if (argc == 3) {
+    vec_files.push_back(argv[2]);
+    read_vec = true;
   }
 
-  #ifdef dbg
-  auto ijk_lambda = [&my_maptrix](const auto &row, const auto &col, const auto &value) {
-    auto &mptrx_comm = my_maptrix.comm();
-    int rank         = mptrx_comm.rank();
-    std::cout << "[MPTRX]: In rank: " << rank << ", key1: " 
-              << row << ", key2: " << col << ", val: " << value << std::endl;
-  };
-  my_maptrix.for_all(ijk_lambda);
-  world.barrier();
+  map_type     x(world);
+  maptrix_type A(world);
 
-  auto map_lambda = [](const auto &res_kv_pair) {
-    std::cout << "[In map lambda] key: " << res_kv_pair.first << ", col: " << res_kv_pair.second << std::endl;
-  };
-  my_map.for_all(map_lambda);
-  world.barrier();
-  #endif
-  
-  auto times_op = ns_spmv::times<double>();
-  auto map_res  = ns_spmv::spmv(my_maptrix, my_map, std::plus<double>(), std::multiplies<double>());
+  world.cout0("Reading maptrix");
+  ygm::io::line_parser line_parser(world, mat_files);
+  line_parser.for_all([&A, &x, read_vec](auto &line) {
+    size_t             src;
+    size_t             dst;
+    int                val = 1;
+    std::istringstream iss(line);
+    iss >> src >> dst >> val;
+    A.async_insert(src, dst, val);
 
-  #ifdef dbg
-  auto print_res_lambda = [](auto res_kv_pair) {
-    std::cout << "[In map res lambda] key: " << res_kv_pair.first << 
-                  ", col: " << res_kv_pair.second << std::endl;
-  };
-  map_res.for_all(print_res_lambda);
-  world.barrier();
-  #endif
-
-  std::cout << std::fixed;
-  std::cout << std::setprecision(8);
-
-  map_type map_gt(world);
-  std::string gt_fname = argv[3];
-  std::ifstream gtfile(gt_fname);
-
-  if (world.rank0()) {
-    while (gtfile >> key1 >> value) {
-      map_gt.async_insert(key1, value);
+    /* Map x. */
+    if (!read_vec) {
+      x.async_insert(src, 1);
+      x.async_insert(dst, 1);
     }
+  });
+
+  world.barrier();
+
+  if (read_vec) {
+    world.cout0("Reading vector");
+    ygm::io::line_parser vec_parser(world, vec_files);
+    vec_parser.for_all([&x](auto line) {
+      size_t             index;
+      size_t             val;
+      std::istringstream iss(line);
+      if (iss >> index >> val) {
+        x.async_insert(index, val);
+      }
+    });
+    world.barrier();
   }
 
-  map_type norm_map(world);
-  norm_map.async_insert(std::string("dist"), 0);
+  world.cout0("Performing SpMV");
+  ygm::timer spmv_timer{};
 
-  auto gt_ptr       = map_gt.get_ygm_ptr();
-  auto norm_map_ptr = norm_map.get_ygm_ptr();
-  auto check_res_lambda = [norm_map_ptr, gt_ptr](auto res_kv_pair) {
+  auto y = ns_spmv::spmv(A, x, std::plus<int>(), std::multiplies<int>());
 
-    auto res_key = res_kv_pair.first;
-    auto res_val = res_kv_pair.second;
-
-    auto visit_ground = [](auto gt_pair, auto res_val, auto norm_map_ptr) {
-
-      auto gt_key = gt_pair.first;
-      auto gt_val = gt_pair.second;
-
-      auto diff = (gt_val - res_val);
-      diff = diff * diff;
-
-      auto accumulate_lambda = [](auto &row_id_val, const auto &update_val) {
-        auto row_id = row_id_val.first;
-        auto value =  row_id_val.second;
-        auto append_val = value + update_val;
-        row_id_val.second = row_id_val.second + update_val;
-      };
-
-      norm_map_ptr->async_insert_if_missing_else_visit(std::string("dist"), diff, accumulate_lambda);
-    };
-
-    gt_ptr->async_visit(res_key, visit_ground, res_val, norm_map_ptr);
-  };
-  map_res.for_all(check_res_lambda);
   world.barrier();
-
-  //if (world.rank0()) {
-    auto print_res_lambda = [&norm_map_ptr](auto res_kv_pair) {
-      auto &comm = norm_map_ptr->comm();
-      auto irank = comm.rank();
-      std::cout << "[In map res lambda] rank: " << irank << " key: " << res_kv_pair.first << ", col: " << sqrt(res_kv_pair.second) << std::endl;
-    };
-    norm_map.for_all(print_res_lambda);
-  //}
+  world.cout0("SpMV time: ", spmv_timer.elapsed(), " seconds");
 
   return 0;
 }
