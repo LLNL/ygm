62c95c2084609f682e057faa667b05652b9fbc53
Trevor Steil <trevor.steil1@gmail.com>
2022-01-07 15:31:38 -0800

    Cleaning up webgraph example to use as general SpMV example

--- examples/webgraph_spmv.cpp
+++ examples/webgraph_spmv.cpp
@@ -3,104 +3,85 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include <fstream>
+//#include <fstream>
+//#include <iomanip>
 #include <iostream>
-#include <iomanip>
 
-#include <math.h>
+//#include <math.h>
+#include <string>
+#include <vector>
 #include <ygm/comm.hpp>
-
+#include <ygm/container/experimental/maptrix.hpp>
+#include <ygm/container/map.hpp>
+#include <ygm/io/line_parser.hpp>
 #include <ygm/utility.hpp>
 
-#include <ygm/container/map.hpp>
-#include <ygm/container/experimental/maptrix.hpp>
-
-#include <ygm/io/line_parser.hpp>
-#include <ygm/io/ndjson_parser.hpp>
-
-#include <boost/tokenizer.hpp>
+//#include <boost/tokenizer.hpp>
 
 int main(int argc, char **argv) {
-
-  std::cout << std::fixed;
-  std::cout << std::setprecision(8);
-
   ygm::comm world(&argc, &argv);
 
-  //using map_type      = ygm::container::map<ssize_t, int>;
-  //using maptrix_type  = ygm::container::experimental::maptrix<ssize_t, int>;
-  
-  using map_type      = ygm::container::map<int32_t, int>;
-  using maptrix_type  = ygm::container::experimental::maptrix<int32_t, int>;
+  using map_type     = ygm::container::map<size_t, int>;
+  using maptrix_type = ygm::container::experimental::maptrix<size_t, int>;
 
-  namespace ns_spmv   = ygm::container::experimental::detail::algorithms;
+  namespace ns_spmv = ygm::container::experimental::detail::algorithms;
 
-  map_type x(world);
-  maptrix_type A(world);
-  
-  auto x_ptr = x.get_ygm_ptr();
-  auto A_ptr = A.get_ygm_ptr(); 
-
-  if(argc == 1) {
+  if (argc == 1) {
     std::cout << "Expected parameter arguments, exiting.." << std::endl;
     exit(0);
   }
 
-  //std::string m_name = argv[1];
-  //std::string v_name = argv[2];
-
-  std::vector<std::string> fnames;
-  for (int i = 1; i < argc; ++i) {
-    std::cout << argv[i] << std::endl;
-    fnames.push_back(argv[i]);
+  std::vector<std::string> mat_files({argv[1]});
+  std::vector<std::string> vec_files;
+  bool                     read_vec{false};
+  if (argc == 3) {
+    vec_files.push_back(argv[2]);
+    read_vec = true;
   }
 
-  ygm::io::line_parser line_parser(world, fnames);
-  //ssize_t src, dst; 
-  int32_t src, dst; 
-  line_parser.for_all([&A, &x, &src, &dst](auto &line) {
-    std::istringstream iss(line);
-    if (iss >> src >> dst) {
-      //std::cout << src << " " << dst << std::endl;
-      /* Maptrix A. */
-      A.async_insert(src, dst, 1);
-      A.async_insert(dst, src, 1);
+  map_type     x(world);
+  maptrix_type A(world);
 
-      /* Map x. */
-      // You might want to use rand here.
+  world.cout0("Reading maptrix");
+  ygm::io::line_parser line_parser(world, mat_files);
+  line_parser.for_all([&A, &x, read_vec](auto &line) {
+    size_t             src;
+    size_t             dst;
+    int                val = 1;
+    std::istringstream iss(line);
+    iss >> src >> dst >> val;
+    A.async_insert(src, dst, val);
+
+    /* Map x. */
+    if (!read_vec) {
       x.async_insert(src, 1);
       x.async_insert(dst, 1);
     }
   });
 
-  #ifdef dbg
-  auto ijk_lambda = [&A](const auto &row, const auto &col, const auto &value) {
-    auto &mptrx_comm = A.comm();
-    int rank         = mptrx_comm.rank();
-    std::cout << "[MPTRX]: In rank: " << rank << ", key1: " 
-              << row << ", key2: " << col << ", val: " << value << std::endl;
-  };
-  A.for_all(ijk_lambda);
   world.barrier();
 
-  auto map_lambda = [](const auto &res_kv_pair) {
-    std::cout << "[In map lambda] key: " << res_kv_pair.first << ", col: " << res_kv_pair.second << std::endl;
-  };
-  x.for_all(map_lambda);
-  world.barrier();
-  #endif
- 
-  world.barrier();
-  ygm::timer spmv_timer{};
-  
-  auto y = ns_spmv::spmv(A, x, std::plus<double>(), std::multiplies<double>());
-
-  world.barrier();
-  double elapsed = spmv_timer.elapsed();
-  if (world.rank() == 0) {
-    std::cout << "LOGGER: " << "Rank: " << world.rank()
-              << ", [MAX] SpMV time: " << elapsed << "s." << std::endl;
+  if (read_vec) {
+    world.cout0("Reading vector");
+    ygm::io::line_parser vec_parser(world, vec_files);
+    vec_parser.for_all([&x](auto line) {
+      size_t             index;
+      size_t             val;
+      std::istringstream iss(line);
+      if (iss >> index >> val) {
+        x.async_insert(index, val);
+      }
+    });
+    world.barrier();
   }
+
+  world.cout0("Performing SpMV");
+  ygm::timer spmv_timer{};
+
+  auto y = ns_spmv::spmv(A, x, std::plus<int>(), std::multiplies<int>());
+
+  world.barrier();
+  world.cout0("SpMV time: ", spmv_timer.elapsed(), " seconds");
 
   return 0;
 }
