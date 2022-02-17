bedce94afbc64816aa01b303818c75c6cae28e8e
Trevor Steil <trevor.steil1@gmail.com>
2022-02-17 09:59:39 -0800

    Adds test for counting_set's YGM pointer

--- test/test_counting_set.cpp
+++ test/test_counting_set.cpp
@@ -55,5 +55,30 @@ int main(int argc, char **argv) {
     ASSERT_RELEASE(cset.count_all() == 3 * (size_t)world.size());
   }
 
+  //
+  // Test counting_sets YGM pointer
+  {
+    ygm::container::counting_set<std::string> cset(world);
+
+    auto cset_ptr = cset.get_ygm_ptr();
+
+    // Mix operations with pointer and counting_set
+    cset_ptr->async_insert("dog");
+    cset_ptr->async_insert("apple");
+    cset.async_insert("red");
+
+    ASSERT_RELEASE(cset_ptr->count("dog") == (size_t)world.size());
+    ASSERT_RELEASE(cset_ptr->count("apple") == (size_t)world.size());
+    ASSERT_RELEASE(cset.count("red") == (size_t)world.size());
+    ASSERT_RELEASE(cset.size() == 3);
+
+    auto count_map = cset.all_gather({"dog", "cat", "apple"});
+    ASSERT_RELEASE(count_map["dog"] == (size_t)world.size());
+    ASSERT_RELEASE(count_map["apple"] == (size_t)world.size());
+    ASSERT_RELEASE(cset.count("cat") == 0);
+
+    ASSERT_RELEASE(cset.count_all() == 3 * (size_t)world.size());
+  }
+
   return 0;
 }
