29cc6adf19523917bc563cfd35930ba5d04b401a
Trevor Steil <trevor.steil1@gmail.com>
2022-12-07 15:57:52 -0800

    Updates Parquet reader tests and examples

--- test/test_arrow_parquet_stream_reader.cpp
+++ test/test_arrow_parquet_stream_reader.cpp
@@ -13,76 +13,69 @@
 int main(int argc, char** argv) {
   ygm::comm world(&argc, &argv);
 
-  // assuming the build directory is inside the YGM root directory 
-  const std::string dir_name = "data/parquet_files/"; 
+  // assuming the build directory is inside the YGM root directory
+  const std::string dir_name = "data/parquet_files/";
 
   //
   // Test number of lines in files
   {
-  // arrow_parquet_parser assumes files have identical scehma
-  ygm::io::arrow_parquet_parser parquetp(world, {dir_name});
+    // arrow_parquet_parser assumes files have identical scehma
+    ygm::io::arrow_parquet_parser parquetp(world, {dir_name});
 
-  // count total number of rows in files   
-  size_t local_count = 0;
-  
-  parquetp.for_all(
-    [&local_count](auto& stream_reader, const auto& field_count) {
-      stream_reader.SkipColumns(field_count);
-      stream_reader.EndRow();
-      local_count++; 
-    }
-  ); 
+    // count total number of rows in files
+    size_t local_count = 0;
 
-  world.barrier();
-  auto row_count = world.all_reduce_sum(local_count);
-  ASSERT_RELEASE(row_count == 12);
+    parquetp.for_all(
+        [&local_count](auto& stream_reader, const auto& field_count) {
+          stream_reader.SkipColumns(field_count);
+          stream_reader.EndRow();
+          local_count++;
+        });
 
+    world.barrier();
+    auto row_count = world.all_reduce_sum(local_count);
+    ASSERT_RELEASE(row_count == 12);
   }
 
   //
   // Test table entries
   {
-  // arrow_parquet_parser assumes files have identical scehma
-  ygm::io::arrow_parquet_parser parquetp(world, {dir_name});
+    // arrow_parquet_parser assumes files have identical scehma
+    ygm::io::arrow_parquet_parser parquetp(world, {dir_name});
 
-  // read fields in each row 
-  struct columns {
-    std::string string_field;
-    char char_array_field[4];
-    uint64_t uint64_t_field;
-    double double_field;
-    bool boolean_field;    
-  };   
+    // read fields in each row
+    struct columns {
+      std::string string_field;
+      char        char_array_field[4];
+      uint64_t    uint64_t_field;
+      double      double_field;
+      bool        boolean_field;
+    };
 
-  std::vector<columns> rows;
+    std::vector<columns>  rows;
+    std::set<std::string> strings;
 
-  parquetp.for_all( 
-    [&rows](auto& stream_reader, const auto& field_count) {
-      using columns_t = decltype(rows)::value_type;
-      columns_t columns_obj;
-      stream_reader >> columns_obj.string_field;
-      stream_reader >> columns_obj.char_array_field;
-      stream_reader >> columns_obj.uint64_t_field;
-      stream_reader >> columns_obj.double_field;
-      stream_reader >> columns_obj.boolean_field;      
-      stream_reader.EndRow();       
-      rows.emplace_back(columns_obj); 
-    }
-  ); 
+    parquetp.for_all(
+        [&rows, &strings](auto& stream_reader, const auto& field_count) {
+          using columns_t = decltype(rows)::value_type;
+          columns_t columns_obj;
+          stream_reader >> columns_obj.string_field;
+          stream_reader >> columns_obj.char_array_field;
+          stream_reader >> columns_obj.uint64_t_field;
+          stream_reader >> columns_obj.double_field;
+          stream_reader >> columns_obj.boolean_field;
+          stream_reader.EndRow();
+          rows.emplace_back(columns_obj);
 
-  world.barrier();
-  auto row_count = world.all_reduce_sum(rows.size());   
-  ASSERT_RELEASE(row_count == 12);
+          strings.insert(columns_obj.string_field);
+        });
 
-  if (world.rank() == 0) { 
-    auto& obj_ref = rows[1];  
-    ASSERT_RELEASE(obj_ref.string_field == "Hennessey Venom F5");
-    ASSERT_RELEASE(std::string(obj_ref.char_array_field) == "USA");
-    ASSERT_RELEASE(obj_ref.uint64_t_field == 311); 
-    ASSERT_RELEASE(obj_ref.double_field == 2.4);
-    ASSERT_RELEASE(obj_ref.boolean_field == false); 
-  }  
+    world.barrier();
+    auto row_count = world.all_reduce_sum(rows.size());
+    ASSERT_RELEASE(row_count == 12);
 
+    ASSERT_RELEASE(world.all_reduce_sum(strings.count("Hennessey Venom F5")) ==
+                   1);
   }
 
   return 0;
