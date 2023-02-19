42e719aee0edf14c063ee3ff9307222633f7434f
Trevor Steil <trevor.steil1@gmail.com>
2023-02-19 10:04:51 -0800

    Stores reduction operation in ygm::container::reducing_adapter

--- include/ygm/container/reducing_adapter.hpp
+++ include/ygm/container/reducing_adapter.hpp
@@ -12,16 +12,15 @@ namespace ygm::container {
 template <typename Container, typename ReductionOp>
 class reducing_adapter {
  public:
-  reducing_adapter(Container &c, ReductionOp reducer) : m_container(c) {}
+  reducing_adapter(Container &c, ReductionOp reducer)
+      : m_container(c), m_reducer(reducer) {}
 
   void async_reduce(const typename Container::key_type   &key,
                     const typename Container::value_type &value) {
-    ReductionOp *r;
-
     if constexpr (ygm::container::detail::is_map<Container>) {
-      m_container.async_reduce(key, value, *r);
+      m_container.async_reduce(key, value, m_reducer);
     } else if constexpr (ygm::container::detail::is_array<Container>) {
-      m_container.async_binary_op_update_value(key, value, *r);
+      m_container.async_binary_op_update_value(key, value, m_reducer);
     } else {
       static_assert(ygm::detail::always_false<>,
                     "Container unsuitable for reducing_adapter");
@@ -29,7 +28,8 @@ class reducing_adapter {
   }
 
  private:
-  Container &m_container;
+  Container  &m_container;
+  ReductionOp m_reducer;
 };
 
 template <typename Container, typename ReductionOp>
