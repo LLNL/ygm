39c6cfa0ef7273abc33e90fb88a3a2cc2a841a57
Stephen Thaddaeus Youd <youd3@catalyst159.llnl.gov>
2023-05-31 13:06:18 -0700

    Updated reducing adapter and it's tests to reflect new container detection

--- include/ygm/container/detail/reducing_adapter.hpp
+++ include/ygm/container/detail/reducing_adapter.hpp
@@ -94,9 +94,14 @@ class reducing_adapter {
   }
 
   void container_reduction(const key_type &key, const value_type &value) {
-    if constexpr (ygm::container::detail::is_map<Container>) {
+    if constexpr(ygm::container::detail::check_ygm_container_type<
+                    Container, 
+                    ygm::container::detail::map_tag>()) {
       m_container.async_reduce(key, value, m_reducer);
-    } else if constexpr (ygm::container::detail::is_array<Container>) {
+
+    } else if constexpr(ygm::container::detail::check_ygm_container_type<
+                    Container, 
+                    ygm::container::detail::array_tag>()) {
       m_container.async_binary_op_update_value(key, value, m_reducer);
     } else {
       static_assert(ygm::detail::always_false<>,
