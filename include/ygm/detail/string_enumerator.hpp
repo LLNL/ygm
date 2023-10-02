59da52301d010ca78117d04f84967b6b0ce2a530
Trevor Steil <trevor.steil1@gmail.com>
2023-10-02 11:21:32 -0700

    Formatting and removing make -j

--- include/ygm/detail/string_enumerator.hpp
+++ include/ygm/detail/string_enumerator.hpp
@@ -10,24 +10,32 @@
 #include <iostream>
 #include <vector>
 
-template <size_t N> struct StringLiteral {
-  constexpr StringLiteral(const char (&str)[N]) { std::copy_n(str, N, value); }
+// Class to hold constexpr string.
+// Constexpr constructor used to allow templates to appear to accept constexpr
+// strings as template parameters (requires C++20 features)
+template <size_t N>
+struct StringLiteral {
+  constexpr StringLiteral<N>(const char (&str)[N]) {
+    std::copy_n(str, N, value);
+  }
 
   char value[N];
 };
 
 class string_enumerator {
-public:
-public:
-  template <StringLiteral S> class entry {
-  public:
-    const static int id;
+ public:
+ public:
+  template <StringLiteral S>
+  class entry {
+   public:
+    const static int   id;
     static inline bool is_set = false;
   };
 
   string_enumerator();
 
-  template <StringLiteral S> static int get_string_index() {
+  template <StringLiteral S>
+  static int get_string_index() {
     if (not entry<S>::is_set) {
       record_string<S>();
     }
@@ -43,8 +51,7 @@ public:
     return m_vec[get_string_index<S>()];
   }
 
-private:
-
+ private:
   template <StringLiteral S>
   static int priv_get_string_index() {
     return entry<S>::id;
@@ -54,16 +61,17 @@ private:
   static void record_string() {
     ASSERT_RELEASE(entry<S>::id < m_vec.size());
     m_vec[priv_get_string_index<S>()] = S.value;
-    entry<S>::is_set = true;
+    entry<S>::is_set                  = true;
   }
 
   static int m_counter;
-public:
+
+ public:
   static std::vector<std::string> m_vec;
 };
 
 std::vector<std::string> string_enumerator::m_vec;
-int string_enumerator::m_counter = 0;
+int                      string_enumerator::m_counter = 0;
 template <StringLiteral S>
 const int string_enumerator::entry<S>::id = string_enumerator::next_counter();
 
