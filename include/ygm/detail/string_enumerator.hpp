d4fa1317b15fd6d88304ff28ca78a26f06880e66
steiltre <trevor.steil1@gmail.com>
2023-10-27 19:45:33 -0700

    Finished changes to maps with string literal keys to work with gcc 9.

--- include/ygm/detail/string_enumerator.hpp
+++ include/ygm/detail/string_enumerator.hpp
@@ -15,65 +15,69 @@
 // strings as template parameters (requires C++20 features)
 template <size_t N>
 struct StringLiteral {
-  constexpr StringLiteral<N>(const char (&str)[N]) {
-    std::copy_n(str, N, value);
+  constexpr StringLiteral<N>(const char (&str)[N]) : value("") {
+    for (size_t i = 0; i < N; ++i) {
+      value[i] = str[i];
+    }
   }
 
-  char value[N];
+  char                    value[N];
+  constexpr static size_t len = N;
 };
 
 class string_enumerator {
  public:
- public:
+  using index_type = int;
+
   template <StringLiteral S>
-  class entry {
-   public:
-    const static int   id;
-    static inline bool is_set = false;
-  };
+  class entry;
 
   string_enumerator();
 
   template <StringLiteral S>
-  static int get_string_index() {
-    if (not entry<S>::is_set) {
+  static index_type get_string_index() {
+    if (not entry<StringLiteral<S.len>(S)>::is_set) {
       record_string<S>();
     }
     return priv_get_string_index<S>();
   }
 
-  int get_num_items() { return m_counter; }
+  index_type get_num_items() { return m_counter; }
 
-  static int next_counter() { return m_counter++; }
+  static index_type next_counter() { return m_counter++; }
+
+  const static std::string &get_string(index_type index) {
+    ASSERT_RELEASE(index < m_counter);
+    return m_vec[index];
+  }
 
   template <StringLiteral S>
-  static std::string get_string() {
-    return m_vec[get_string_index<S>()];
-  }
+  class entry {
+   public:
+    const static inline index_type id     = string_enumerator::next_counter();
+    static inline bool             is_set = false;
+  };
 
  private:
   template <StringLiteral S>
-  static int priv_get_string_index() {
-    return entry<S>::id;
+  static index_type priv_get_string_index() {
+    return entry<StringLiteral<S.len>(S)>::id;
   }
 
   template <StringLiteral S>
   static void record_string() {
-    ASSERT_RELEASE(entry<S>::id < m_vec.size());
-    m_vec[priv_get_string_index<S>()] = S.value;
-    entry<S>::is_set                  = true;
+    ASSERT_RELEASE(entry<StringLiteral<S.len>(S)>::id < m_vec.size());
+    m_vec[priv_get_string_index<S>()]      = S.value;
+    entry<StringLiteral<S.len>(S)>::is_set = true;
   }
 
-  static int m_counter;
+  static index_type m_counter;
 
- public:
   static std::vector<std::string> m_vec;
 };
 
-std::vector<std::string> string_enumerator::m_vec;
-int                      string_enumerator::m_counter = 0;
-template <StringLiteral S>
-const int string_enumerator::entry<S>::id = string_enumerator::next_counter();
+std::vector<std::string>      string_enumerator::m_vec;
+string_enumerator::index_type string_enumerator::m_counter = 0;
 
 string_enumerator::string_enumerator() {
   string_enumerator::m_vec.resize(string_enumerator::m_counter);
