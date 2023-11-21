489b633ae89ee41b511c0523a9595fa1830ef6d1
steiltre <trevor.steil1@gmail.com>
2023-11-20 17:26:43 -0800

    Adds missing license header

--- include/ygm/stats_tracker.hpp
+++ include/ygm/stats_tracker.hpp
@@ -1,3 +1,8 @@
+// Copyright 2019-2021 Lawrence Livermore National Security, LLC and other YGM
+// Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: MIT
+
 #pragma once
 
 #include <functional>
