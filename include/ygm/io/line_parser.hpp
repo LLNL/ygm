28677520f7dce45388e22d969eab8e7fd5cf4d8e
Trevor Steil <steil1@llnl.gov>
2024-08-01 15:05:27 -0700

    CSV Parser Headers (#214)
    * Adds ability to handle headers in CSV files. Requires allowing ygm::io::line_parser to read the first line of the first file independently and skip over the first line of files in for_all operations.
    
    * Makes ygm::io::detail::csv_line::size() const
    
    * Adds ability to check if ygm::io::csv_parser headers contain a particular label
    
    * Fixes use of ygm::comm::mpi_bcast() in ygm::io::line_parser::read_first()
    
    * Makes skipping first line in line parser controlled by a member variable

--- include/ygm/io/line_parser.hpp
+++ include/ygm/io/line_parser.hpp
@@ -32,7 +32,9 @@ class line_parser: public ygm::container::detail::base_iteration<line_parser, st
    */
   line_parser(ygm::comm& comm, const std::vector<std::string>& stringpaths,
               bool node_local_filesystem = false, bool recursive = false)
-      : m_comm(comm), m_node_local_filesystem(node_local_filesystem) {
+      : m_comm(comm),
+        m_node_local_filesystem(node_local_filesystem),
+        m_skip_first_line(false) {
     if (node_local_filesystem) {
       ASSERT_RELEASE(false);
       check_paths(stringpaths, recursive);
@@ -124,21 +126,43 @@ class line_parser: public ygm::container::detail::base_iteration<line_parser, st
         ASSERT_RELEASE(ifs.good());
         ifs.imbue(std::locale::classic());
         std::string line;
+        bool        first_line = false;
         // Throw away line containing bytes_begin as it was read by the previous
         // process (unless it corresponds to the beginning of a file)
         if (bytes_begin > 0) {
           ifs.seekg(bytes_begin);
           std::getline(ifs, line);
+        } else {
+          first_line = true;
         }
         // Keep reading until line containing bytes_end is read
         while (ifs.tellg() <= bytes_end && std::getline(ifs, line)) {
-          fn(line);
+          // Skip first line if necessary
+          if (not first_line || not m_skip_first_line) {
+            fn(line);
+          } else {
+          }
           // if(ifs.tellg() > bytes_end) break;
+          first_line = false;
         }
       }
       my_file_paths.clear();
     }
   }
+
+  std::string read_first_line() {
+    std::string line;
+    if (m_comm.rank0()) {
+      std::ifstream ifs(m_paths[0]);
+      std::getline(ifs, line);
+    }
+
+    line = m_comm.mpi_bcast(line, 0, m_comm.get_mpi_comm());
+
+    return line;
+  }
+
+  void set_skip_first_line(bool skip_first) { m_skip_first_line = skip_first; }
 
  private:
   /**
@@ -212,6 +236,7 @@ class line_parser: public ygm::container::detail::base_iteration<line_parser, st
   ygm::comm&            m_comm;
   std::vector<fs::path> m_paths;
   bool                  m_node_local_filesystem;
+  bool                  m_skip_first_line;
 };
 
 }  // namespace ygm::io
